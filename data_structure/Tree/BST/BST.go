3893cf9f28034bc9c542115ee54dcfe891d55423
xlapt <viclx1223@gmail.com>
2019-01-29 15:52:12 -0500

    update

--- data_structure/Tree/BST/BST.go
+++ data_structure/Tree/BST/BST.go
@@ -7,10 +7,8 @@ package main
 import "fmt"
 
 type node struct {
-	key   int
-	val   int
-	left  *node
-	right *node
+	Key, Val int
+	Left, Right  *node
 }
 
 type BST struct {
@@ -24,111 +22,134 @@ func max(a, b int) int {
 	return b
 }
 
-func newNode(val, key int) *node {
-	return &node{key, val, nil, nil}
+func newNode(Val, Key int) *node {
+	return &node{Key, Val, nil, nil}
+}
+
+func New() BST {
+	return BST{}
 }
 
 func inorder(n *node) {
 	if n != nil {
-		inorder(n.left)
-		fmt.Print(n.val, " ")
-		inorder(n.right)
+		inorder(n.Left)
+		fmt.Println(n.Key, ":", n.Val)
+		inorder(n.Right)
 	}
 }
 
-func Search(root *node, key int) int {
+func search(root *node, Key int) int {
 	if root == nil {
 		return -1
 	}
-	if root.key < key {
-		return Search(root.left, key)
-	} else if root.key > key{
-		return Search(root.right, key)
+	if root.Key > Key {
+		return search(root.Left, Key)
+	} else if root.Key < Key{
+		return search(root.Right, Key)
 	} else {
-		return root.val
+		return root.Val
 	}
 }
-func insert(root *node, val, key int) *node {
+
+
+func (t *BST) Search(Key int)  {
+	fmt.Println(search(t.root, Key))
+}
+
+func insert(root *node,  Key, Val int) *node {
 	if root == nil {
-		return newNode(val, key)
+		return newNode(Val, Key)
 	}
-	if key < root.key {
-		root.left = insert(root.left, val, key)
+	if Key < root.Key {
+		root.Left = insert(root.Left, Key, Val)
 	} else {
-		root.right = insert(root.right, val, key)
+		root.Right = insert(root.Right, Key, Val)
 	}
 	return root
 }
 
-func inorderSuccessor(root *node) *node {
+func (t *BST) Insert(Key, Val int) {
+	t.root = insert(t.root, Key, Val)
+}
+func leftMost(root *node) *node {
 	cur := root
-	for cur.left != nil {
-		cur = cur.left
+	for cur.Left != nil {
+		cur = cur.Left
 	}
 	return cur
 }
 
-func bst_delete(root *node, key int) *node {
+func (t *BST) Delete(Key int) {
+	t.root = bst_delete(t.root, Key)
+}
+
+func bst_delete(root *node, Key int) *node {
 	if root == nil {
 		return nil
 	}
-	if key < root.key {
-		root.left = bst_delete(root.left, key)
-	} else if key > root.key {
-		root.right = bst_delete(root.right, key)
+	if Key < root.Key {
+		root.Left = bst_delete(root.Left, Key)
+	} else if Key > root.Key {
+		root.Right = bst_delete(root.Right, Key)
 	} else {
 		// this is the node to delete
 
 		// node with one child
-		if root.left == nil {
-			root = root.right
-		} else if root.right == nil {
-			root = root.left
+		if root.Left == nil {
+			root = root.Right
+		} else if root.Right == nil {
+			root = root.Left
 		} else {
 			// node with two children
-			d := inorderSuccessor(root.right)
-			root.val, root.key = d.val, d.key
-			root.right = bst_delete(root.right, d.key)
+			d := leftMost(root.Right)
+			root.Val, root.Key = d.Val, d.Key
+			root.Right = bst_delete(root.Right, d.Key)
 		}
 	}
 	return root
 }
 
-// helper function for t.depth
 func _calculate_depth(n *node, depth int) int {
 	if n == nil {
 		return depth
 	}
-	return max(_calculate_depth(n.left, depth+1), _calculate_depth(n.right, depth+1))
+	return max(_calculate_depth(n.Left, depth+1), _calculate_depth(n.Right, depth+1))
 }
 
 func (t *BST) depth() int {
 	return _calculate_depth(t.root, 0)
 }
 
+func (t *BST) Show()  {
+	inorder(t.root)
+}
 
 func main() {
 	t := &BST{nil}
-	inorder(t.root)
-	t.root = insert(t.root, 10)
-	t.root = insert(t.root, 20)
-	t.root = insert(t.root, 15)
-	t.root = insert(t.root, 30)
-	fmt.Print(t.depth(), "\n")
-	inorder(t.root)
-	fmt.Print("\n")
-	t.root = bst_delete(t.root, 10)
-	inorder(t.root)
-	fmt.Print("\n")
-	t.root = bst_delete(t.root, 30)
-	inorder(t.root)
-	fmt.Print("\n")
-	t.root = bst_delete(t.root, 15)
-	inorder(t.root)
-	fmt.Print("\n")
-	t.root = bst_delete(t.root, 20)
-	inorder(t.root)
-	fmt.Print("\n")
-	fmt.Print(t.depth(), "\n")
+	t.Insert(10, 1)
+	t.Insert(20, 1)
+	t.Insert(12, 1)
+	t.Insert(22, 1)
+	t.Insert(30, 1)
+	t.Insert(11, 1)
+	t.Insert(60, 1)
+	//t.Show()
+	t.Search(10)
+	t.Delete(10)
+	t.Show()
+	t.Search(10)
+	//t.root = bst_delete(t.root, 10)
+	//inorder(t.root)
+	//fmt.Print("\n")
+	//t.root = bst_delete(t.root, 30)
+	//inorder(t.root)
+	//fmt.Print("\n")
+	//t.root = bst_delete(t.root, 15)
+	//inorder(t.root)
+	//fmt.Print("\n")
+	//t.root = bst_delete(t.root, 20)
+	//inorder(t.root)
+	//fmt.Print("\n")
+	//fmt.Print(t.depth(), "\n")
 }
 
