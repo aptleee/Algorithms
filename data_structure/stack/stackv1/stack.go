844e408e8235a3fbb0c3e781e232c7474142889a
xlapt <viclx1223@gmail.com>
2019-02-03 11:51:44 -0500

    2/3/2019

--- data_structure/stack/stackv1/stack.go
+++ data_structure/stack/stackv1/stack.go
@@ -4,39 +4,25 @@ import "fmt"
 
 type Stack struct {
 	array []int
-	size  int
 	top   int
 }
 
 func NewStack(size int) Stack {
 	return Stack{
 		array: make([]int, size),
-		size:  size,
 		top:   0,
 	}
 }
 
 func (st *Stack) Push(item int) {
-	if st.Full() == false {
-		st.array[st.top] = item
-		st.top++
-	}
+	st.array[st.top] = item
+	st.top++
 }
 
 func (st *Stack) Pop() int {
-	if st.Empty() == false {
-		st.top--
-		v := st.array[st.top]
-		return v
-	}
-	return -100
-}
-
-func (st *Stack) Full() bool {
-	if st.top == st.size {
-		return true
-	}
-	return false
+	st.top--
+	v := st.array[st.top]
+	return v
 }
 
 func (st *Stack) Empty() bool {
@@ -52,7 +38,6 @@ func main() {
 	for i := 0; i < 5; i++ {
 		st.Push(i)
 	}
-	fmt.Println(st.Full())
 	fmt.Println(st.Pop())
 	st.Push(6)
 	for i := 0; i < 5; i++ {
