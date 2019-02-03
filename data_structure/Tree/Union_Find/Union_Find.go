5c43588e1ba033140b4ece75437fde5d11266bcf
xlapt <viclx1223@gmail.com>
2019-02-03 11:45:39 -0500

    2/3/2019

--- data_structure/Tree/Union_Find/Union_Find.go
+++ data_structure/Tree/Union_Find/Union_Find.go
@@ -15,17 +15,36 @@ func New(n int) *UF {
 	}
 	return u
 }
+
 func (u *UF) find(i int) int{
 	for i != u.friends[i]{
 		i = u.friends[i]
 	}
 	return i
 }
-func (u *UF) union(i, j int) {
+
+func (u *UF) qucikUnion(i, j int) { //quick union
 	ir := u.find(i)
 	jr := u.find(j)
 	if ir != jr {
 		u.count--
 		u.friends[ir] = jr
 	}
+}
+
+func (u *UF) quickFind(i int) int {
+	return u.friends[i]
+}
+
+func (u *UF) union(i, j int) {
+	ir := u.quickFind(i)
+	jr := u.quickFind(j)
+	if ir != jr {
+		for k := 0; k < len(u.friends); k++ {
+			if u.friends[k] == ir {
+				u.friends[k] = jr
+			}
+		}
+	}
+	u.count--
 }
