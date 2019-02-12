f33918dbdb1abbfc5af835e287bb338d626fc1ed
xlapt <viclx1223@gmail.com>
2019-02-12 18:41:49 -0500

    2/12/2019

--- lock/dining_philosopher.go
+++ lock/dining_philosopher.go
@@ -1,10 +1,17 @@
 package lock
 
 
-
 var chopsticks = [5]semaphore{}
 
+func I() {
+	for _, e := range chopsticks {
+		e = new(1)
+	}
+}
+
+// if 5 philosophers pick up chopstick[i] at the same time, there is deadlock
 func loop(i int) {
+
 	for {
 		chopsticks[i].Wait()
 		chopsticks[(i+1)%5].Wait()
@@ -18,4 +25,13 @@ func loop(i int) {
 		// thinking
 	}
 
+}
+// remedies
+// 1. only allow four philosophers to pick up chopsticks at the same time
+// 2. philosopher will only pick chopsticks when there are two available
+// 3. odd numbered philosopher first pick up i and then pick up i+1;
+//    even numbered philosopher first pick up i+1 and then i
+
+func main()  {
+	loop(0)
 }
--- lock/peterson_algorithem.go
+++ lock/peterson_algorithem.go
@@ -13,3 +13,36 @@ func lock(i int) {
 func unlock(i int) {
 	flag[i] = 0
 }
+
+// simple spin lock implementation
+// flag = 0
+// func test&set(flag)  {
+// 	rv = flag
+//	flag = 1
+//	return rv
+// }
+
+// func lock() {
+//	for test&set(flag) == 1 {}
+// }
+//
+// func unlock() {
+// 	flag = 0
+// }
+
+// func compare&swap(v ,expected, new_v int) {
+//	tmp := v
+//	if v == expected {
+//		v = new_v
+//	}
+// 	return tmp
+// }
+//
+// lock = 0
+// func lock(){
+// 	for compare&swap(v, 0, 1) != 0 {}
+// }
+// func unlock() {
+//	lock = 0
+// }
+
