3942b316d15da7685c8d039661eff77a07551ee0
xlapt <viclx1223@gmail.com>
2019-02-26 17:16:51 -0500

    2/26/2019

--- mystring/substring.go
+++ mystring/substring.go
@@ -17,5 +17,36 @@ func bruteForce(s, p string) int {
 }
 
 func KMP(s, p string) int {
-	dfa := make([][]int, 0)
+	// when there is a match, so next position of j is j+1, we get dfa[][j] = j+1
+	// when there is a mismatch, we know not just s[i],
+	// but also the j-1 previous characters in the text: they are the first j-1 characters in the pattern.
+	// so we slide a copy of p on the j characters, and move from left to right, until there is a match
+	// in this way we can find the next position of p to be compared with next character in s
+	//
+	R, m := 256, len(p)
+	dfa := make([][]int, R) // dfa[c][j] is the position of p to compare against next s , after compare c with p[j]
+	for i := 0; i < R; i++ {
+		dfa[i] = make([]int, m)
+	}
+
+	dfa[p[0]][0] = 1
+	for x, j := 0, 1; j < m; j++ {
+		for c := 0; c < R; c++ {
+			dfa[c][j] = dfa[c][x]
+		}
+		dfa[p[j]][j] = j+1
+		x = dfa[p[j]][x]
+	}
+
+	search := func(s string) int {
+		i, j, n, m := 0, 0, len(s), len(p)
+		for ; i < n && j < m; i++ {
+			j = dfa[s[i]][j] // move j according to dfa[][j]
+		}
+		if j == m {
+			return i - m
+		}
+		return n
+	}
+	return search(s)
 }
